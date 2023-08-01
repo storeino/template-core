5e7e83d9f8f55210bef8d071bab485c610ed40dc
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2023-08-01 13:57:02 +0100

    update template core links

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const plugins = [
-  '@/node_modules/storeino-template-core/plugins/tools.js',
-  '@/node_modules/storeino-template-core/plugins/http.js',
-  '@/node_modules/storeino-template-core/plugins/storeino.js',
-  '@/node_modules/storeino-template-core/plugins/init.js',
-  '@/node_modules/storeino-template-core/plugins/events.js',
+  '@/node_modules/@storeino/template-core/plugins/tools.js',
+  '@/node_modules/@storeino/template-core/plugins/http.js',
+  '@/node_modules/@storeino/template-core/plugins/storeino.js',
+  '@/node_modules/@storeino/template-core/plugins/init.js',
+  '@/node_modules/@storeino/template-core/plugins/events.js',
 ];
 
 module.exports = { plugins };
